00bb1bbc8d27439360276a11644ae9fac88fe715
seshasui <seshaui@gmail.com>
2018-02-16 19:26:11 -0500

    First COmmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import generateBarcode from 'pdf417/build';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
+
+  public generateBarcode = generateBarcode;
+
+   public imageSrc;
+   public formData = {};
+   public showBarcode: any = false;
+
+   public generateBarcodeData = () => {
+    this.imageSrc = generateBarcode(JSON.stringify(this.formData), 10, 3);
+    this.showBarcode = true;
+   }
+
+  /* private generateBarCodeData = (formData) => {
+      return this.generateBarcode(JSON.stringify(formData), 10, 3);
+  } */
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
